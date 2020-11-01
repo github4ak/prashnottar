088b40e64c3b9550c1391e9dd50cde9d11f64b0f
Abishek Krishnan <github4ak@gmail.com>
2020-11-01 00:27:32 -0600

    Modify py program to generate required i/o and test f score

--- qa.py
+++ qa.py
@@ -22,14 +22,16 @@ def main(argv):
     # Read the input file
     with open(argv[0], 'r') as f:
         input_file_contents = f.readlines()
-
     directory_path = input_file_contents[0].rstrip()
 
     # Create list of stories together with its questions
     stories = get_story_objects(directory_path, input_file_contents)
 
-    # Print formatted ouput
+    # Print formatted ouput and make the response file
     print_formatted_output(stories)
+
+    # Make the perfect answer file
+    make_perfect_answer(directory_path, input_file_contents)
 
 
 def get_story_objects(directory_path, input_file_contents):
@@ -69,17 +71,56 @@ def get_story_objects(directory_path, input_file_contents):
 
 
 def print_formatted_output(stories):
+
+    output_filename = "my_custom_list.response"
+
+    # Clear the file
+    open(output_filename, "w").close()
+
+    output_file_content = ""
+
     for story in stories:
         for q in story.story_questions:
-            print("Question ID: " + q.question_id)
+            question_id_response = "QuestionID: " + q.question_id
+            print(question_id_response)
+            output_file_content += question_id_response + "\n"
             answer = get_answer(story.text, q.question_text)
-            print("Answer: " + answer + "\n")
+            answer_response = "Answer: " + answer + "\n"
+            print(answer_response)
+            output_file_content += answer_response + "\n"
+
+    # Write the output
+    with open(output_filename, "a") as f3:
+        f3.write(output_file_content.rstrip())
 
 
 def get_answer(story_text, question_text):
 
     # TO ADD LOGIC HERE
     return "insert-answer-string"
+
+
+def make_perfect_answer(directory_path, input_file_contents):
+
+    output_filename = "my_custom_list.answers"
+
+    # Clear the file
+    open(output_filename, "w").close()
+
+    output_file_content = ""
+
+    for i in range(1, len(input_file_contents)):
+        answer_file_path = directory_path + "\\" + \
+            str(input_file_contents[i]).rstrip() + ".answers"
+        # Create answers
+        with open(answer_file_path, 'r') as a:
+            answer_content = a.readlines()
+            for answer_line in answer_content:
+                output_file_content += answer_line
+
+    # Write the output
+    with open(output_filename, "a") as f3:
+        f3.write(output_file_content.rstrip())
 
 
 if __name__ == "__main__":
